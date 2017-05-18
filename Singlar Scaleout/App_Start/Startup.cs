c3b317fc4282a07a23a6efffe66dbca18a8451e7
MarsCaiWORD <gary759521@gmail.com>
2017-05-18 09:43:25 +0800

    加入專案檔案。

---
+++ Singlar Scaleout/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Singlar_Scaleout.App_Start.Startup))]
+
+namespace Singlar_Scaleout.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalHost.DependencyResolver.UseRedis("127.0.0.1" , 6379, string.Empty, "測試");
+            app.MapSignalR();
+        }
+    }
+}
---
+++ Singlar Scaleout/ServiceHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Singlar_Scaleout
+{
+    public class ServiceHub :Hub
+    {
+
+        [HubMethodName("SendMessage")]
+        public void SendMessage(string msg) {
+
+            Clients.All.goPage(msg);
+        }
+    }
+}
